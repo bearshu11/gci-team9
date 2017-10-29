1fd2678d1913e22ebdd8043060ab045b8b9c46d1
y.naoki <noknok7710@gmail.com>
2017-10-29 17:56:45 +0900

    Add files via upload

---
+++ yasutake_everyone/mf.py
@@ -0,0 +1,21 @@
+from sklearn.decomposition import NMF
+import numpy as np
+from numpy import nan
+A = np.array([
+        [5, nan, nan, nan,nan,nan],
+        [4, nan, nan, 1,8,nan],
+        [3, 1, nan, 5,9,7],
+        [3, nan, nan, 4,7,8],
+        [nan, 1, 6, 4,2,6],
+        [nan,7,7,nan,3,0]
+        ]
+    )
+model = NMF(n_components=50,solver='mu', tol=0.1,max_iter=100,init='random',random_state=0)
+P = model.fit_transform(A)
+Q = model.components_
+class MatrixFactorization():
+    def factorize(self,R):
+        model = NMF(n_components=50,solver='mu', tol=0.1,max_iter=100,init='random',random_state=0)
+        P = model.fit_transform(R)
+        Q = model.components_
+        return P,Q
